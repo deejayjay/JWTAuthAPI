70a3d10587e74999355ce82a25d8b63c600d8f26
DeeJayJay <deejayjay@zoho.com>
2023-07-25 18:08:54 -0600

    Completes the code which generates JWT

---
+++ JWTAuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,113 @@
+﻿using JWTAuthAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JWTAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDTO newUser)
+        {
+            CreatePasswordHash(newUser.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = newUser.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDTO objUser)
+        {
+            if (user.Username != objUser.Username)
+            {
+                return BadRequest("Incorrect username.");
+            }
+
+            if (!VerifyPasswordHash(objUser.Password, user.PasswordHash!, user.PasswordSalt!))
+            {
+                return BadRequest("Incorrect password.");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        #region Helper methods
+
+        /// <summary>
+        /// Create token for the given user.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new()
+            { 
+                new Claim(ClaimTypes.Name, user.Username)                
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        /// <summary>
+        /// Generate a password hash and salt from a given password.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <param name="passwordSalt"></param>
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        /// <summary>
+        /// Verify that the password hash matches with the existing hash.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="passwordHash"></param>
+        /// <param name="passwordSalt"></param>
+        /// <returns></returns>
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        #endregion
+    }
+}
